5dcfd47a1b51a63ec4a150ebab7dda0b43683452
btc8b <157297636+btc8b@users.noreply.github.com>
2024-03-10 07:20:54 +0100

    Update btc8b.py

--- btc8b.py
+++ btc8b.py
@@ -1,13 +1,18 @@
 #learning python
+import cmath
+import numpy
+
 i =1
 j =3
 k = i + j
 print("i+j=",k)
-import cmath
 c=complex(i,j)
 print(c.real)
 print("the real part of c = ",c.real)
 print("the imaginary part of c = ",c.imag)
 absc=abs(c)
 print("abs(c)=",abs(c))
-print("abs(c)=",phase(c))
+
+
+arr = numpy.array([1, 2, 3, 4, 5])
+print(arr)
