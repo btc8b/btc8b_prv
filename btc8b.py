4fad9137ca9a28af6513b70a15a48e330c11d56e
btc8b <157297636+btc8b@users.noreply.github.com>
2024-03-17 11:26:29 +0100

    Update btc8b.py

--- btc8b.py
+++ btc8b.py
@@ -16,8 +16,9 @@ print("abs(c)=",abs(c))
 
 arr = numpy.array([1, 2, 3, 4, 5])
 print(arr)
-
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
 print(arr[1:5])
 print(arr[:4])
 print(arr[-3:-1])
+
+for x in arr:
+  print(x)
