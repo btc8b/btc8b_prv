7a9290680487ed4a7f229b1c2dbaec08a1e9cb57
btc8b <157297636+btc8b@users.noreply.github.com>
2024-03-22 16:09:01 +0100

    Update btc8b.py

--- btc8b.py
+++ btc8b.py
@@ -43,3 +43,10 @@ print(x)
 x = random.chisquare(df=2, size=(2, 3))
 print(x)
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.chisquare(df=1, size=1000), hist=False)
+
+plt.show()
+
